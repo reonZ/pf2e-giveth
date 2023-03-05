1b3f3c22a018581efb860870ad684a52742685f8
Idle <651462+reonZ@users.noreply.github.com>
2023-03-05 22:45:09 +0100

    only one gm runs the requests

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { registerSetting } from '@utils/foundry/settings'
 import { onDropCanvasData } from './canvas'
 import { onDropActorSheetData } from './sheet'
 import { takethCondition, takethEffect, takethPhysical } from './taketh'
+import { isFirstGM } from '@utils/foundry/user'
 
 export const MODULE_ID = 'pf2e-giveth'
 setModuleID(MODULE_ID)
@@ -28,6 +29,7 @@ Hooks.once('ready', () => {
 })
 
 function onPacketReceived(packet: Packet) {
+    if (!isFirstGM()) return
     if (packet.type === 'giveth-condition') takethCondition(packet)
     else if (packet.type === 'giveth-effect') takethEffect(packet)
     else takethPhysical(packet)
